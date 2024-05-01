c8c2e17426e285851b69ec98cf5c62ac189614d1
Robert Antony <antonyrobert202@gmail.com>
2024-05-01 23:39:36 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    int x=10;
+    int *y=&x;
+    
+    
+    std::cout<<++*y;
+
+    return 0;
+}
+
+
+//*y is used to mention that the variable is a pointer which doesn't hold any value, but it holds the address of the other variable, so if any operation is done on the pointer will occur in the pointing variable
